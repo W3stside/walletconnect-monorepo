1059dce6928b27750c05453933b5a3c52cd95804
Pedro Gomes <pedrogomes94@gmail.com>
2018-10-22 20:09:42 -0700

    mobile events

--- packages/walletconnect-react-native/src/index.js
+++ packages/walletconnect-react-native/src/index.js
@@ -1,7 +1,35 @@
 import checkPushOptions from './checkPushOptions'
+import WalletConnector from './walletConnector'
 
 export default class RNWalletConnectWallet {
   constructor(opts) {
     this.push = checkPushOptions(opts)
+    this.walletConnectors = {}
   }
+
+  setWalletConnector(sessionId, walletConnector) {
+    this.walletConnectors[sessionId] = walletConnector
+    return true
+  }
+
+  getWalletConnector(sessionId) {
+    const walletConnector = this.walletConnectors[sessionId]
+    return walletConnector
+  }
+
+  generateWalletConnector(session) {
+    const walletConnector = new WalletConnector({ ...session, push: this.push })
+    this.setWalletConnector(walletConnector.sessionId, walletConnector)
+  }
+
+  onQRCodeScan(uri) {
+    this.generateWalletConnector({ uri })
+  }
+
+  onDeepLink(link) {
+    const uri = 'ethereum:' + link.substring(link.indexOf(':') + 1)
+    this.generateWalletConnector({ uri })
+  }
+
+  onPushNotification() {}
 }
